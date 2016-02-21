164e6b9105a37d130ae54000b4fb26e584cfb729
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-21 02:57:44 +0200

    init_msm: Allow vendor init to override props
    Change-Id: Ic7c4d86616bfe33b9668b5b54d119d0380f4660c

--- modules/init/init_msm.cpp
+++ modules/init/init_msm.cpp
@@ -26,8 +26,21 @@
  */
 
 #include <sys/sysinfo.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
 #include "property_service.h"
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
 char const *heapstartsize;
 char const *heapgrowthlimit;
