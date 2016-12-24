58b0b19900e84d0f7c33cd7e8151a5f173eb453b
M1cha <sigmaepsilon92@gmail.com>
2016-12-24 16:43:42 +0100

    product: camera: cleanup

--- product/camera.mk
+++ product/camera.mk
@@ -1,14 +1,8 @@
-# Camera
 PRODUCT_PACKAGES += \
     Snap
-
-# Disable Camera TNR
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.tnr.preview=0 \
-    persist.camera.tnr.video=0
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
-    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml \
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml
