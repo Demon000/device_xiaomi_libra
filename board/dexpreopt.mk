b7b20f30eb85c68714a216ca7562f6c52131cea5
M1cha <sigmaepsilon92@gmail.com>
2016-12-24 11:23:34 +0100

    enable dexpreopt

---
+++ board/dexpreopt.mk
@@ -0,0 +1,8 @@
+ # Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifeq ($(TARGET_BUILD_VARIANT),user)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
