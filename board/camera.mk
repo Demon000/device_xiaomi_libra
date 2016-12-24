fdca71e4b9e014cc77e4a0ece7a8b24321b97515
M1cha <sigmaepsilon92@gmail.com>
2016-12-24 19:28:13 +0100

    board: camera: use correct flags for our old HAL

--- board/camera.mk
+++ board/camera.mk
@@ -1,4 +1,6 @@
-# Force camera module to be compiled only in 32-bit mode on 64-bit systems
-# Once camera module can run in the native mode of the system (either
-# 32-bit or 64-bit), the following line should be deleted
-BOARD_QTI_CAMERA_32BIT_ONLY := true
+USE_DEVICE_SPECIFIC_CAMERA := true
+TARGET_USES_MEDIA_EXTENSIONS := true
+
+# compat
+BOARD_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
