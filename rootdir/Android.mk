99f7480480d90544c1b12979e4f61aaaeb15bb71
M1cha <sigmaepsilon92@gmail.com>
2016-12-24 05:30:44 +0100

    remove init.libra.qseecomd.sh

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -83,14 +83,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.libra.qseecomd.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := bin/init.libra.qseecomd.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.libra.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
