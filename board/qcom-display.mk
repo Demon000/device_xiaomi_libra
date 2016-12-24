549f2d0b7cc7a3283aa8bfe1636940876127f125
M1cha <sigmaepsilon92@gmail.com>
2016-12-24 10:45:30 +0100

    board: qcom-display: cleanup

--- board/qcom-display.mk
+++ board/qcom-display.mk
@@ -1,21 +1,10 @@
-# Shader cache config options
-# Maximum size of the  GLES Shaders that can be cached for reuse.
-# Increase the size if shaders of size greater than 12KB are used.
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
-
-# Maximum GLES shader cache size for each app to store the compiled shader
-# binaries. Decrease the size if RAM or Flash Storage size is a limitation
-# of the device.
 MAX_EGL_CACHE_SIZE := 2048*1024
 
-OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
 USE_OPENGL_RENDERER := true
-
-TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
-MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
-
-BOARD_EGL_CFG := $(DEVICE_PATH)/configs/egl.cfg
-HAVE_ADRENO_SOURCE:= false
