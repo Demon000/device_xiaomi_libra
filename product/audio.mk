579f7c83b96788990e8c457abcb8a41c7d75869e
M1cha <sigmaepsilon92@gmail.com>
2016-12-24 16:09:39 +0100

    improve audio config

--- product/audio.mk
+++ product/audio.mk
@@ -1,19 +1,29 @@
-# Audio HAL and utilities
-USE_XML_AUDIO_POLICY_CONF := 1
+# Audio
 PRODUCT_PACKAGES += \
-    audio.primary.msm8992 \
     audio.a2dp.default \
-    audio.usb.default \
+    audio.primary.msm8992 \
+    audio_policy.msm8992 \
     audio.r_submix.default \
-    libaudio-resampler
+    audio.usb.default
+
+# Amplifier
+PRODUCT_PACKAGES += \
+    audio_amplifier.msm8992
+
+# some extra tools
+PRODUCT_PACKAGES += \
+    tinycap \
+    tinymix \
+    tinypcminfo \
+    tinyplay
 
 # routes, paths, effects, policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes_drc.xml:system/etc/audio_policy_volumes_drc.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # features
 PRODUCT_COPY_FILES += \
@@ -22,27 +32,21 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml  \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:system/etc/permissions/android.hardware.audio.pro.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-# Low latency audio buffer size in frames
+# fluence multi-mic solution
 PRODUCT_PROPERTY_OVERRIDES += \
-    audio_hal.period_size=192
-
-# Reduce client buffer size for fast audio output tracks
-PRODUCT_PROPERTY_OVERRIDES += \
-    af.fast_track_multiplier=1
-
-# Audio
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qc.sdk.audio.fluencetype=fluencepro \
+    persist.audio.dirac.speaker=true \
+    ro.qc.sdk.audio.fluencetype=fluence \
     persist.audio.fluence.voicecall=true \
-    persist.audio.fluence.voicecomm=true \
     persist.audio.fluence.voicerec=false \
     persist.audio.fluence.speaker=true \
-    persist.speaker.prot.enable=true \
-    ro.config.vc_call_vol_steps=7
+
+# stereo camcorder
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.audio.camcorder.stereo=true
 
 # low audio flinger standby delay to reduce power consumption
 PRODUCT_PROPERTY_OVERRIDES += \
--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -7,3 +7,26 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
+
+# speaker protection
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.speaker.prot.enable=true \
+    persist.spkr.cal.duration=0
+
+# surround sound recording
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qc.sdk.audio.ssr=false \
+    persist.audio.ssr.3mic=false
+
+# offload settings
+PRODUCT_PROPERTY_OVERRIDES += \
+    audio.offload.gapless.enabled=true \
+    audio.offload.buffer.size.kb=32 \
+    audio.offload.video=true \
+    audio.offload.pcm.16bit.enable=false \
+    audio.offload.pcm.24bit.enable=true \
+    audio.offload.multiple.enabled=false
+
+# voip
+PRODUCT_PROPERTY_OVERRIDES += \
+    use.voice.path.for.pcm.voip=true
