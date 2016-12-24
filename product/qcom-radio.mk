35ab38bc83dcfdf45f49d326b573e53c3066bfb1
M1cha <sigmaepsilon92@gmail.com>
2016-12-24 15:33:35 +0100

    product: qcom-radio: cleanup

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -1,77 +1,23 @@
 PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
-
+    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    ril.subscription.types=RUIM
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.default_cdma_sub=0 \
-    persist.qcril.disable_retry=true
-
-# LTE, CDMA, GSM/WCDMA
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.default_network=22 \
+    persist.data.mode=concurrent \
+    persist.data.netmgrd.qos.enable=true \
+    persist.data.tcp_rst_drop=true \
+    persist.radio.apm_sim_not_pwdn=1 \
+    ro.data.large_tcp_window_size=true \
+    ro.use_data_netmgrd=true \
     telephony.lteOnCdmaDevice=1 \
-    persist.radio.mode_pref_nv10=1
-
-# Emergency call overrides for Korea
-PRODUCT_PROPERTY_OVERRIDES += \
-    ril.nosim.ecc_list_count=1 \
-    ril.nosim.ecc_list_1=111,113,117,122,125
-
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.apm_sim_not_pwdn=1
-
-# Setup custom emergency number list based on the MCC. This is needed by RIL
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.custom_ecc=1
-
-# Ril sends only one RIL_UNSOL_CALL_RING, so set call_ring.multiple to false
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.call_ring.multiple=0
+    persist.radio.data_no_toggle=1 \
+    persist.radio.rat_on=combine \
+    ro.telephony.default_network=20
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
-# Request modem to send PLMN name always irrespective
-# of display condition in EFSPN.
-# RIL uses this property.
+# Multi SIM
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.always_send_plmn=true
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.use_cc_names=true
-
-# Update 1x signal strength after 10s
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.radio.snapshot_enabled=1 \
-    persist.radio.snapshot_timer=10
-
-# If data_no_toggle is 0 there are no reports if the screen is off.
-# If data_no_toggle is 1 then dormancy indications will come with screen off.
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.data_no_toggle=1
-
-
-# Adjust STK popup operation
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.process_sups_ind=1
-
-# VZW voice roaming issue
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.eri64_as_home=1
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.data_con_rprt=true
-
-# Incoming number (b/23529711)
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.redir_party_num=0
-
-# Modem debugger/misc
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-ifeq (,$(filter aosp_libra, $(TARGET_PRODUCT)))
-PRODUCT_PACKAGES += \
-    QXDMLoggerV2
-endif # aosp_libra
-endif
+    persist.radio.multisim.config=dsds
