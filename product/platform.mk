0b9c1411b8abfbdb6cb10be7029d59c78ea6dd4b
M1cha <sigmaepsilon92@gmail.com>
2016-12-24 15:53:11 +0100

    product: platform: cleanup

--- product/platform.mk
+++ product/platform.mk
@@ -6,14 +6,3 @@ PRODUCT_PACKAGES += \
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
-
-PRODUCT_TAGS += dalvik.gc.type-precise
-PRODUCT_CHARACTERISTICS := nosdcard
-
-# For android_filesystem_config.h
-PRODUCT_PACKAGES += \
-   fs_config_files
-
-# Storage
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sys.sdcardfs=true
