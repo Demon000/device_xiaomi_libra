270cdf3c9011e8c97d4085e7694ec37c8a0fa645
M1cha <sigmaepsilon92@gmail.com>
2016-12-24 15:25:52 +0100

    product: qcom-display: cleanup

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -1,17 +1,34 @@
+# Display HAL
 PRODUCT_PACKAGES += \
+    copybit.msm8992 \
     gralloc.msm8992 \
     hwcomposer.msm8992 \
-    libgenlock \
+    liboverlay \
+    libqdutils \
+    libqdMetaData \
     memtrack.msm8992
 
+# QC CABL feature
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qualcomm.cabl=0
+
+# MSM8992 HAL settings
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.composition.type=c2d \
+    debug.egl.hw=1 \
+    debug.mdpcomp.logs=0 \
+    debug.sf.hw=1 \
+    persist.debug.wfd.enable=1 \
+    persist.demo.hdmirotationlock=false \
+    persist.hwc.enable_vds=1 \
     persist.hwc.mdpcomp.enable=true \
-    ro.opengles.version=196610
+    persist.hwc.ptor.enable=true \
+    persist.mdpcomp.4k2kSplit=1 \
+    persist.mdpcomp_perfhint=50 \
+    persist.metadata_dynfps.disable=false \
+    persist.sys.wfd.virtual=0 \
+    ro.opengles.version=196609
 
 # OpenGLES Android Extension
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml
-
-# For SPN display
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/spn-conf.xml:system/etc/spn-conf.xml
