80fe713b84a95b3f49669f55d33ad6d4223edebc
M1cha <sigmaepsilon92@gmail.com>
2016-12-26 09:46:05 +0100

    product: qcom-misc: move msm_irqbalance.conf to vendor/etc

--- product/qcom-misc.mk
+++ product/qcom-misc.mk
@@ -1,6 +1,6 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/etc/msm_irqbalance.conf
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
 
 PRODUCT_PACKAGES += \
     libtinyxml
