7a417bfe78e2ab8259da235efef8429e86e0e78e
M1cha <sigmaepsilon92@gmail.com>
2017-01-02 08:45:31 +0100

    audio: update configs from CAF and drop dolby changes

--- product/audio.mk
+++ product/audio.mk
@@ -19,6 +19,7 @@ PRODUCT_PACKAGES += \
 
 # routes, paths, effects, policy
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:system/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
