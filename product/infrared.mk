e251eebfa10af4d24d89ab42f6b035b0fe70869d
M1cha <sigmaepsilon92@gmail.com>
2017-01-02 22:52:08 +0100

    add consumerir HAL

--- product/infrared.mk
+++ product/infrared.mk
@@ -1,2 +1,5 @@
+PRODUCT_PACKAGES += \
+    consumerir.msm8992
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
