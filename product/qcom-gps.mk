435efd87a33e422b3a8a3debd13850e94ef3a3ef
M1cha <sigmaepsilon92@gmail.com>
2017-01-04 12:48:06 +0100

    move all libraries and executables sources to modules/

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -2,10 +2,10 @@ PRODUCT_PACKAGES += \
     gps.msm8992
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf
+    $(LOCAL_PATH)/modules/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/modules/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/modules/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/modules/gps/etc/sap.conf:system/etc/sap.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
