9d008fd803f3e78f124d518474339bd1d4179ee4
M1cha <sigmaepsilon92@gmail.com>
2017-01-04 13:01:35 +0100

    cm.mk: remove fingerprint overrides
    we don't have any OEM apps, so we don't really need this

--- cm.mk
+++ cm.mk
@@ -24,6 +24,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=libra \
-    BUILD_FINGERPRINT="google/libra/libra:7.1.1/NMF26F/3425388:user/release-keys" \
-    PRIVATE_BUILD_DESC="libra-user 7.1.1 NMF26F 3425388 release-keys"
+    PRODUCT_NAME=libra
