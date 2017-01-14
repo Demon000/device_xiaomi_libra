326f218d7ef95088db071088adb3e248825a9a81
M1cha <sigmaepsilon92@gmail.com>
2017-01-14 06:58:30 +0100

    Revert "product: display: remove unused vulkan permissions"
    This reverts commit b452e4b7e9e3f6e955d514d04ba4f1de0d339a86.

--- product/display.mk
+++ product/display.mk
@@ -1,3 +1,7 @@
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
