0e7654836e3d8d4a3e9a844c5861213d16534bd4
M1cha <sigmaepsilon92@gmail.com>
2017-01-14 07:00:51 +0100

    product: display: add comment to vulkan stuff

--- product/display.mk
+++ product/display.mk
@@ -2,6 +2,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480
 
+# vulkan
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
