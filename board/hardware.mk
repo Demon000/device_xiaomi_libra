9d0e45069a016b7400c7d87cea7aec59090a442f
Demon000 <demonsingur@gmail.com>
2017-03-02 18:41:28 +0200

    add KeyDisabler

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,7 +1,8 @@
 # CM Hardware
 BOARD_USES_CYANOGEN_HARDWARE := true
 BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw
+    hardware/cyanogen/cmhw \
+    device/xiaomi/libra/cmhw
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
