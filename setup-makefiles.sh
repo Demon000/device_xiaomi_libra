0b032e5d8fc76e57e0458ad5e295c3e0a217ef65
Carlos Solano <csolano@gmail.com>
2017-03-03 13:57:35 +0100

    Remove qcom binaries from extract script
    Change-Id: I557f572deee92d5119c00af87c512f9b3e6e98a0

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -43,7 +43,5 @@ write_headers
 # The standard blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt
 
-printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8992/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
-
 # We are done!
 write_footers
