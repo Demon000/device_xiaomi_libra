5a8b124463afb27390b0d6362b347ed225454350
AndropaX <kpax.mar@gmail.com>
2017-04-05 21:39:11 +0200

    enable vr
    Change-Id: I1aaf6c370801969373cd9fd12e9363dc5fce87cf

--- product/display.mk
+++ product/display.mk
@@ -1,4 +1,5 @@
 # vulkan
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml \
+    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:system/etc/permissions/android.hardware.vr.high_performance.xml
