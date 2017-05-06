d5ebfe35de5b0a6fa699186e62e5ede748330934
Demon000 <demonsingur@gmail.com>
2017-05-06 21:09:00 +0300

    move libra makefile out of lineage.mk
    Change-Id: Ie33faf5fd0dce7cfeefc0fbeadb7db0e122aa6d3

--- full_libra.mk
+++ full_libra.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
-#               2017 The LineageOS Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,12 +14,18 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/xiaomi/libra/full_libra.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/libra/device.mk)
 
-PRODUCT_NAME := lineage_libra
-BOARD_VENDOR := Xiaomi
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := full_libra
+PRODUCT_DEVICE := libra
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=libra
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi-4c
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_RESTRICT_VENDOR_FILES := false
