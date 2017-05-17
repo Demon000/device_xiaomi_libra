fa3115c6b3dd22c17d0916008637a688fe15c664
Demon000 <demonsingur@gmail.com>
2017-05-17 21:13:57 +0300

    post_boot: decrease minfree values
    Change-Id: I1653c58829d45c02024567b16113a8274aef2eb6

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -42,7 +42,7 @@ function configure_memory_parameters() {
     echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
 
     if [ "$arch_type" == "aarch64" ] && [ $MemTotal -gt 2097152 ]; then
-        echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
+        echo "14746,18432,22118,25805,33038,41988" > /sys/module/lowmemorykiller/parameters/minfree
         echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
     elif [ "$arch_type" == "aarch64" ] && [ $MemTotal -gt 1048576 ]; then
         echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
