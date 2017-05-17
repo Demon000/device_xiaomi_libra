a99dac3b3caf51ade88aefb7806b655b4ed70f7d
Demon000 <demonsingur@gmail.com>
2017-05-17 20:34:21 +0300

    post_boot: set min_cpus to 0
    Change-Id: Ib16694c940e9ccbcc305f19b52aae94b30df3337

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -177,7 +177,7 @@ echo "0:787200 1:787200" > /sys/module/cpu_boost/parameters/input_boost_freq
 echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms
 
 # core_ctl module
-echo 1 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
+echo 0 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
 echo 60 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
 echo 30 > /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres
 echo 100 > /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms
