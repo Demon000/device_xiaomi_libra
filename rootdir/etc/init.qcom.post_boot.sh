e4491a664a6cb2349d9add504e8b5d6ac650f0c1
Demon000 <demonsingur@gmail.com>
2017-05-31 08:15:54 +0000

    init.target.rc: configure per profile input boost
    Change-Id: Ibce9bbabbfc58799bb9590cfab432c80222cdd81

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -129,11 +129,6 @@ echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
 # Restore CPU 4 max freq from msm_performance
 echo "4:4294967295 5:4294967295" > /sys/module/msm_performance/parameters/cpu_max_freq
 
-# input boost configuration
-echo 1 > /sys/module/cpu_boost/parameters/input_boost_enabled
-echo "0:787200 1:787200" > /sys/module/cpu_boost/parameters/input_boost_freq
-echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms
-
 # core_ctl module
 echo 0 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
 echo 2 > /sys/devices/system/cpu/cpu4/core_ctl/max_cpus
