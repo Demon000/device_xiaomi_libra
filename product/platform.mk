95f6513ce69da707074c462add2a51ad904438d6
Demon000 <demonsingur@gmail.com>
2017-06-30 19:23:39 +0300

    rootdir: remove usf_post_boot.sh
    Change-Id: I9ef40a9f9e97d1c08a37bed7330ace5d8081104b

--- product/platform.mk
+++ product/platform.mk
@@ -3,10 +3,6 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     ueventd.qcom.rc
 
-# etc files
-PRODUCT_PACKAGES += \
-    usf_post_boot.sh
-
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
