013496f7dc24ea4326bbddfc2b6a08141cf9ddcf
Demon000 <demonsingur@gmail.com>
2017-07-12 16:06:39 +0300

    gps: move configs to config dir
    We aren't using source built GPS anymore.
    
    Change-Id: Ibd038bc479ade8e8a4bdd28fbe31c3767feaa944

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,8 +1,8 @@
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/modules/gps/etc/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/modules/gps/etc/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/modules/gps/etc/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/modules/gps/etc/sap.conf:system/etc/sap.conf
+    $(LOCAL_PATH)/configs/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/configs/gps/sap.conf:system/etc/sap.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
