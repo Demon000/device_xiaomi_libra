b8c9f7a16cd1bdc1241a624a98309d38b90172ed
Demon000 <demonsingur@gmail.com>
2017-07-12 16:10:22 +0300

    input: remove edge touch keylayouts
    We don't support those kernel-side anymore, there's
    no reason to keep them.
    
    Change-Id: I3be55a0747b064a0c728abe0c912d1b701ec6591

--- product/input.mk
+++ product/input.mk
@@ -1,11 +1,9 @@
 # Input device files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/atmel-maxtouch.kl:system/usr/keylayout/atmel-maxtouch.kl \
-    $(LOCAL_PATH)/configs/atmel-maxtouch-edge.kl:system/usr/keylayout/atmel-maxtouch-edge.kl \
     $(LOCAL_PATH)/configs/ft5x46.kl:system/usr/keylayout/ft5x46.kl \
     $(LOCAL_PATH)/configs/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/configs/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
-    $(LOCAL_PATH)/configs/synaptics_dsx_edge.kl:system/usr/keylayout/synaptics_dsx_edge.kl
+    $(LOCAL_PATH)/configs/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
