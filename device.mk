2a20d48cc31bae7f4efd4db1c958f7b48ba2146f
Demon000 <demonsingur@gmail.com>
2017-08-29 10:26:55 +0300

    display: adapt to O AOSP HALs
    Change-Id: Ib391f99634a5e7dc3fe36448e23c69e917885596

--- device.mk
+++ device.mk
@@ -118,14 +118,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    copybit.msm8992 \
     gralloc.msm8992 \
     hwcomposer.msm8992 \
-    liboverlay \
-    libqdutils \
-    libqdMetaData \
     memtrack.msm8992 \
+    libgenlock \
     libtinyxml
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.renderscript@1.0-impl
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
