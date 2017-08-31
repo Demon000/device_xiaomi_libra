a5b944250913ed0d64bd6240cf3441c7bd63e9a3
Artem Borisov <dedsa2002@gmail.com>
2017-08-31 16:41:25 +0300

    camera: add atomic calls shim
    01-21 03:47:04.275   509   509 F libc    : CANNOT LINK EXECUTABLE "/system/bin/mm-qcamera-daemon": cannot locate symbol "android_atomic_acquire_load" referenced by "/system/vendor/lib/libmmcamera2_stats_algorithm.so"...
    
    Change-Id: Ia875a5385931d5e79bfc1a99c3d8996ac66de0df

--- device.mk
+++ device.mk
@@ -98,6 +98,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
+    libshim_atomic \
     Snap
 
 PRODUCT_COPY_FILES += \
--- modules/libshim/Android.mk
+++ modules/libshim/Android.mk
@@ -12,3 +12,9 @@ LOCAL_SRC_FILES := rild_socket.c
 LOCAL_MODULE := rild_socket
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := atomic.cpp
+LOCAL_MODULE := libshim_atomic
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_SHARED_LIBRARY)
