8fa2f532a3b1815c69e94b9f473b973068d5136c
Demon000 <demonsingur@gmail.com>
2017-10-11 22:41:00 +0300

    proprietary-files: use bullhead gps stack
    Change-Id: I57ebd506264e1d5f22b8a6cf679dd7190014684b

--- device.mk
+++ device.mk
@@ -154,15 +154,16 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    libshims_get_process_name \
-    libshims_is_wifi_driver_loaded \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl \
+    libgps.utils \
+    gps.msm8992
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/configs/gps/sap.conf:system/etc/sap.conf
+    $(LOCAL_PATH)/configs/gps/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/configs/gps/sap.conf:system/etc/lowi.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
--- modules/libshim/Android.mk
+++ modules/libshim/Android.mk
@@ -18,16 +18,3 @@ LOCAL_SRC_FILES := atomic.cpp
 LOCAL_MODULE := libshim_atomic
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := get_process_name.c
-LOCAL_MODULE := libshims_get_process_name
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := is_wifi_driver_loaded.cpp
-LOCAL_MODULE := libshims_is_wifi_driver_loaded
-LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := libwifi-hal
-include $(BUILD_SHARED_LIBRARY)
