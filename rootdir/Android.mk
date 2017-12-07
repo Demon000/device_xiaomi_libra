5b22989fb99c349c8fc930376b9fe4d93f46eb62
harrynowl <harrynowl@gmail.com>
2017-12-07 19:32:32 +0000

    rootdir: add fpc init file
    Change-Id: I2b63cda94c698c72e1873fbe6edbea74a1670163

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -66,6 +66,13 @@ LOCAL_SRC_FILES    := ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.fpc.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.fpc.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
 
 # recovery
 
