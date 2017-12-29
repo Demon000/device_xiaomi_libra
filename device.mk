LineageOS/android_device_xiaomi_libra

vibrator: replace cmhw intensity with lineage interface
Change-Id: I05a535d143ea9a91b6302a56212b3f92029132d6

device.mk
@@ -298,7 +298,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-impl.lineage
 
 # WiFi
 PRODUCT_PACKAGES += \

Demon000/device_xiaomi_libra

device.mk
@@ -299,7 +299,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.lineage
 
 # WiFi
 PRODUCT_PACKAGES += \