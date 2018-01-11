78b06de4b7e3f44376fab0f795e31e7e1da6a892
Michael Bestas <mkbestas@lineageos.org>
2018-01-11 11:51:16 +0200

    libra: include legacy qcom sepolicy
    Change-Id: Ib6fad8188d6a9c3fdf9892050da6f73ee2dc805a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,6 +160,7 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Vendor init
