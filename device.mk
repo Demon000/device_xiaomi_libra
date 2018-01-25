32dede20c95938c535c6da3f111bf69298bbbf60
Dan Pasanen <dan.pasanen@gmail.com>
2018-01-25 15:25:56 +0000

    device: build telephony-ext
    This has been removed from vendor/lineage, but is needed
    on devices that ship qti-telephony-common.jar.
    
    Change-Id: I11f38218b64b4cedb2354db627d46de86fe4f549

--- device.mk
+++ device.mk
@@ -295,6 +295,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml
 
 # Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
