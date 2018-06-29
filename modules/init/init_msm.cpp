1b7e95493113295aad8dcde13cc487565edb48ff
Demon Singur <demonsingur@gmail.com>
2018-06-29 08:33:19 +0000

    init: override props for both system and vendor
    Change-Id: I582a1b7bb0b787d5a82e3192796642bee5f7e913

--- modules/init/init_msm.cpp
+++ modules/init/init_msm.cpp
@@ -114,17 +114,17 @@ void vendor_load_properties()
 
     switch(board_id) {
     case LIBRA_BOARD_ID:
-        property_override("ro.product.model", "Mi4c");
-        property_override("ro.product.device", "libra");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "Mi4c");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "libra");
         property_override("ro.build.description", "libra-user 7.0 NRD90M 7.12.28 release-keys");
-        property_override("ro.build.fingerprint", "Xiaomi/libra/libra:7.0/NRD90M/7.12.28:user/release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Xiaomi/libra/libra:7.0/NRD90M/7.12.28:user/release-keys");
         property_override("ro.build.product", "libra");
         break;
     case AQUA_BOARD_ID:
-        property_override("ro.product.model", "Mi4s");
-        property_override("ro.product.device", "aqua");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "Mi4s");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "aqua");
         property_override("ro.build.description", "aqua-user 7.0 NRD90M 7.12.28 release-keys");
-        property_override("ro.build.fingerprint", "Xiaomi/aqua/aqua:7.0/NRD90M/7.12.28:user/release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Xiaomi/aqua/aqua:7.0/NRD90M/7.12.28:user/release-keys");
         property_override("ro.build.product", "aqua");
         property_set("ro.boot.fingerprint", "fpc");
         break;
