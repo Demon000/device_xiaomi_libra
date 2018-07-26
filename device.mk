e2dba10a58a3071f1db18ea3fb6d6e517ea0acda
Demon Singur <demonsingur@gmail.com>
2018-07-26 09:14:37 +0000

    libra: build sound service
    Change-Id: I08f59e37081f0c967f195201beef4506a58e35cd

--- device.mk
+++ device.mk
@@ -65,6 +65,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.soundtrigger@2.0-impl
 
